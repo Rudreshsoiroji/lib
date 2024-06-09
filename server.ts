97a30c58fc5e4a2c167e47fd526770eeedb41223
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-10 02:45:36 +0530

    add port

--- server.ts
+++ server.ts
@@ -1,3 +1,15 @@
-console.log('====================================');
-console.log("hello world");
-console.log('====================================');
+import { log } from "console";
+import app from "./src/app";
+
+const startServer = () => {
+
+    const port = process.env.PORT || 3000
+
+    app.listen(port , ()=>{
+
+     console.log(`The app is connected on port ${port}`);
+        
+    })
+}
+
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import  express  from "express";
+
+const app = express();
+
+app.get('/', (req, res , next) => {
+
+
+    res.json({
+        message: "Hello world"
+    })
+})
+
+
+export default app;
