60ba506ba1237ab0361f4f4a0179a78753d3f4f3
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-10 08:15:40 +0530

    add error handle

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express"
 import createHttpError from "http-errors";
 import userModelSchems from "./userModelSchems";
 import bcrypt from "bcrypt"
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { Users } from "./userTypes";
 const userController = async(req: Request ,res: Response,next:NextFunction) =>{
 
     const { name, email, password } = req.body;
@@ -12,19 +15,51 @@ const userController = async(req: Request ,res: Response,next:NextFunction) =>{
     return next(error);
     }
 
-const user = await userModelSchems.findOne({email})
-if(user){
-    const error = createHttpError(400,"email anready exists");
-    next(error);
+
+// check unique email
+
+try {
+    
+    const user = await userModelSchems.findOne({email})
+    if(user){
+        const error = createHttpError(400,"email anready exists");
+        next(error);
+    }
+} catch (error) {
+   return next(createHttpError(500,"error with the server"))
+   }
+   
+   // hash the oassword using rounds
+   const hashedPass = await bcrypt.hash(password, 10)
+   
+   let newUser:Users;
+
+   try {
+       
+       //create user in database
+        newUser = await userModelSchems.create({
+           name,
+           email,
+           password:hashedPass
+           })
+           } catch (error) {
+               
+               return next(createHttpError(500,"error with the server"))
 }
 
-bcrypt
-
-const hashedPass = await bcrypt.hash(password, 10)
-
-    res.json({
-        messager:"post on register"
-    })
+try {
+    
+    //token generation
+    const token = sign({sub:newUser._id}, config.jasonSecret as string , {expiresIn:"7d"});
+    
+    //response
+        res.json({
+           accessToken: token
+        });
+} catch (error) {
+    return next(createHttpError(500, "error while sending the jwt token"))
+    
+}
 }
 
 export {userController}
