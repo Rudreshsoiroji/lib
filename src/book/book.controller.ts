0651115f5800931f48c71b9c4df5cc7d96033627
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-10 18:51:59 +0530

    create book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -2,9 +2,12 @@ import { NextFunction, Request, Response } from "express"
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import  fs  from "node:fs";
 
 const createBook = async   (req: Request ,res: Response,next:NextFunction) => {
   
+    const {title, genre} = req.body;
   
   
     //upload to cloudinary
@@ -54,11 +57,33 @@ try {
     
     
 }
-    console.log("upload result :" ,uploadResult);
-    console.log("upload result :" ,fileUploadResult);
+//craete a model in database
+
+    
+   const newBook = await bookModel.create({
+    
+        author:"66666978d8337d9bd1c45ee4",
+        coverImage: uploadResult.secure_url,
+        file: fileUploadResult.secure_url,
+        genre,
+        title,
+    });
+
+
+//delete temp files
+try {
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+    
+} catch (error) {
+    console.log(error);
+    
+    
+}
+
     
 
-    res.json({})
+    res.status(201).json({id: newBook._id})
 }
 
 export default createBook;
